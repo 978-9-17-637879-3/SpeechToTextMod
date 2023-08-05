d98c0e348cf3342d2513c905c9c4163232f7bd9f
978-9-17-637879-3 <102838865+978-9-17-637879-3@users.noreply.github.com>
2023-08-05 16:29:00 -0400

    Update mod info

--- src/main/java/git/nineseveneight/speechtotext/SpeechToText.java
+++ src/main/java/git/nineseveneight/speechtotext/SpeechToText.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package git.nineseveneight.speechtotext;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = SpeechToText.MODID, version = SpeechToText.VERSION)
+public class SpeechToText
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "speechtotext";
+    public static final String VERSION = "0.0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
